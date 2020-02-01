e364518474f593077cc0c75a9676fe0852686edc
Radek <radek.halfaf@gmail.com>
2020-02-01 13:38:33 +0100

    add string format for command validation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import calculator.Calculator;
+import calculator.UnsuportedCommandException;
 
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.inputDate();
+        try {
+            calculator.inputDate();
+
+        } catch (UnsuportedCommandException e) {
+            System.out.println("Nie poprawna komenda");
+        }
+
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,23 +2,33 @@ package calculator;
 
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
     double value = 0;
+    public UnsuportedCommandException exeption;
 
-    public void inputDate() {
+    public void inputDate() throws UnsuportedCommandException {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Proszę podać działanie i liczbę ");
+        Pattern pattern = Pattern.compile("[+,\\-,*,\\/] .?\\d+");
         String command = sc.nextLine();
+        Matcher matcher = pattern.matcher(command);
+        boolean checkString = matcher.matches();
         double value = execute(command);
-        System.out.println(value);
+
         if (command.equals("exit")) {
             System.out.println("Rezultat to : " + value);
+        }
+        else if (!checkString){
+            throw new UnsuportedCommandException();
         }
         else {
             inputDate();
         }
+
     }
 
 
