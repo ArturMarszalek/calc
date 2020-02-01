fcfcc68c5914c329920ad15681d2c506f7cda20d
Rafał Załęski <rafalzaleski@gmail.com>
2020-02-01 10:46:22 +0100

    Execute method tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,8 +2,22 @@ package calculator;
 
 
 public class Calculator {
-
+    double value = 0;
     public double execute(String command) {
+
+    String[] tablica = command.split(" ");
+
+
+        switch (tablica[0]) {
+            case "+":
+                return value =+ Double.parseDouble(tablica[1]);
+            case "-":
+                return value =- Double.parseDouble(tablica[1]);
+            case "*":
+                return value = value * Double.parseDouble(tablica[1]);
+            case "/":
+                return value = value / Double.parseDouble(tablica[1]);
+        }
         return 0;
     }
 }
