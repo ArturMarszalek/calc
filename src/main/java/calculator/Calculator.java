804a371d8ead99da6ccc05c26adc86324d3a0e90
kwiatas83 <kwiatas83@gmail.com>
2020-02-01 11:34:08 +0100

    ImplementacjaMetodyExecute

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,8 +2,29 @@ package calculator;
 
 
 public class Calculator {
+    double result;
 
     public double execute(String command) {
-        return 0;
+        String[] splittedString = command.split(" ");
+        double numberFromSplitting = Double.parseDouble(splittedString[1]);
+        switch (splittedString[0]) {
+            case "+":
+                result += numberFromSplitting;
+                return result;
+            case "-":
+                result -= numberFromSplitting ;
+                return result;
+            case "*":
+                result *= numberFromSplitting;
+                return result;
+            case "/":
+                result /= numberFromSplitting;
+                return result;
+            default:
+                return 0;
+        }
+
     }
+
+
 }
