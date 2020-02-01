015638a44f403e8ec6e45040ada75aaf7650b73c
Radek <radek.halfaf@gmail.com>
2020-02-01 10:11:52 +0100

    execute method implementation

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,14 +2,26 @@ package calculator;
 
 
 public class Calculator {
-
+    double value = 0;
     public double execute(String command) {
 
+        String[] calculation = command.split(" ");
+        switch (calculation[0]) {
+            case "+":
+                value += Double.parseDouble(calculation[1]);
+                break;
+            case "-":
+                value -= Double.parseDouble(calculation[1]);
+                break;
+            case "*":
+                value *= Double.parseDouble(calculation[1]);
+                break;
+            case "/":
+                value /= Double.parseDouble(calculation[1]);
+                break;
+        }
 
 
-
-
-
-        return 0;
+        return value;
     }
 }
