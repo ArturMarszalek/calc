0f0097ab9553c3bfb44a9907bfa471390f56bec0
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-02-01 12:17:59 +0100

    try - catch

--- src/main/java/calculator/CalculatorMain.java
+++ src/main/java/calculator/CalculatorMain.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class CalculatorMain {
     public static void main(String[] args) {
-Calculator calculator = new Calculator();
-        Scanner scanner=new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
         boolean loop = true;
         while (loop) {
             System.out.println("Działanie: ");
             String dzialanie = scanner.nextLine();
-if (dzialanie.equals("exit")){
-    loop=false;
-}
-            System.out.println(calculator.execute(dzialanie));
+            if (dzialanie.equals("exit")) {
+                loop = false;
+            }
+            try {
+                System.out.println(calculator.execute(dzialanie));
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Nazwa nie poprawna");
+            }
         }
     }
 }
