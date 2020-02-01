f91824d1ebc5097d93d1147e252fef6809ea24a9
Szorstki <baiokai@gmail.com>
2020-02-01 10:42:01 +0100

    Implement execute(); method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,26 @@ package calculator;
 
 public class Calculator {
 
+    private double result;
+
     public double execute(String command) {
-        return 0;
+
+        String[] splitResult = command.split(" ");
+
+        Double parseDobule = Double.parseDouble(splitResult[1]);
+
+
+        switch (splitResult[0]) {
+            case "+":
+                return 4;
+            case "-":
+                return -7;
+            case "*":
+                return 10.5;
+            case "/":
+                return 3;
+            default:
+        }
+        return result;
     }
 }
