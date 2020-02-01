2e082ae5c184be580ceede36a28a89f55eefc153
Pawel Mikucki <pawel_mikucki@wp.pl>
2020-02-01 10:52:31 +0100

    implement execute method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,33 @@
 package calculator;
 
 
+import java.util.Scanner;
+
 public class Calculator {
+   private double value = 0;
 
     public double execute(String command) {
-        return 0;
+
+
+        String[] splittedCommand = command.split(" ");
+
+        String operator = splittedCommand[0];
+        String number = splittedCommand[1];
+
+        switch (operator) {
+            case "+":
+                value += Double.parseDouble(number);
+                break;
+            case "-":
+                value -= Double.parseDouble(number);
+                break;
+            case "*":
+                value *= Double.parseDouble(number);
+                break;
+            case "/":
+                value /= Double.parseDouble(number);
+                break;
+        }
+        return value;
     }
 }
