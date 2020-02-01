9935b29dbf83c1eb3437a4b1cdddf8359a41ca40
Bartosz Arnista <Bartosz.arni@gmail.com>
2020-02-01 10:43:25 +0100

    Add Execute Method

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,37 @@
 package calculator;
 
 
+import java.sql.SQLOutput;
+
 public class Calculator {
 
+    double wynik = 0;
+
     public double execute(String command) {
-        return 0;
+
+
+        String[] split = command.split(" ");
+
+        switch (split[0]) {
+                case "+":
+                wynik += Double.parseDouble(split[1]);
+                break;
+
+                case "-":
+                wynik -= Double.parseDouble(split[1]);
+                break;
+
+                case "*":
+                wynik *= Double.parseDouble(split[1]);
+                break;
+
+                case "/":
+                wynik /= Double.parseDouble(split[1]);
+                break;
+
+        }
+
+        return wynik;
+
     }
 }
