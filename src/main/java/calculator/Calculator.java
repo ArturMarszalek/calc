1a306dba61d8328335a8eb5494ed9f517182698b
Piotr Suhak <p.suhak@gmail.com>
2020-02-01 10:31:10 +0100

    kalkulator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,28 @@ package calculator;
 
 public class Calculator {
 
+    double result;
+
     public double execute(String command) {
-        return 0;
+        String[] splittedString = command.split( " ");
+
+
+        switch (splittedString[0]) {
+            case ("+"):
+                result = Double.valueOf(splittedString[1]);
+                return result;
+            case "-":
+                result = Double.valueOf(splittedString[1]) * -1;
+                return result;
+            case "*":
+                result = result*Double.valueOf(splittedString[1]);
+                return result;
+            case "/":
+                result = result/Double.valueOf(splittedString[1]);
+                return result;
+            default:
+                return 0;
+        }
     }
 }
+
