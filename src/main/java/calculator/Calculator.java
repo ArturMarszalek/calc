f0a8f1f269f458194e97d0961445e33cf3875b2b
Piotr Suhak <p.suhak@gmail.com>
2020-02-01 14:39:03 +0100

    Create pattern

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,19 +1,29 @@
 package calculator;
 
 
-public class Calculator {
+import java.util.regex.Pattern;
 
+public class Calculator {
     double result;
 
-    public double execute(String command) {
+    public void validateOperation(String command) throws UnsupportedPatternOpperation {
+        Pattern pattern = Pattern.compile("[+\\-*/] \\d*\\.?\\d*");
+        if (!pattern.matcher(command).matches()) {
+            throw new UnsupportedPatternOpperation();
+        }
+    }
+
+    public double execute(String command) throws UnsupportedPatternOpperation {
+        validateOperation(command);
         String[] splittedString = command.split(" ");
         double numberFromSplitting = Double.parseDouble(splittedString[1]);
+
         switch (splittedString[0]) {
             case "+":
                 result += numberFromSplitting;
                 return result;
             case "-":
-                result -= numberFromSplitting ;
+                result -= numberFromSplitting;
                 return result;
             case "*":
                 result *= numberFromSplitting;
@@ -24,13 +34,10 @@ public class Calculator {
             default:
                 return 0;
         }
+
     }
 
-    public double total() {
-        System.out.println(result);
+    public double getResult() {
         return result;
     }
 }
-
-
-
