db29fe43103bd15a140939e0a4e8864f3f120d7f
Wojciech Palmowski <palmowski.wojciech@o2.pl>
2020-02-01 10:43:15 +0100

    kalkulator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,29 @@ package calculator;
 
 public class Calculator {
 
+    double result = 0;
+
     public double execute(String command) {
-        return 0;
+
+        String[] tab = command.split(" ");
+
+        switch (tab[0]) {
+            case "+":
+                result += Double.parseDouble(tab[1]);
+
+                break;
+            case "-":
+                result -= Double.parseDouble(tab[1]);
+                break;
+            case "*":
+                result *= Double.parseDouble(tab[1]);
+                break;
+            case "/":
+                result /= Double.parseDouble(tab[1]);
+                break;
+
+        }
+
+        return result;
     }
 }
