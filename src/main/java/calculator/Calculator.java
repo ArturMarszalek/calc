2e5b00eb0543fe4af0de437b1c32bac8e923e181
Gosia <ingridtatoo@gmail.com>
2020-02-01 10:38:30 +0100

    Uncommented tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,37 @@
 package calculator;
 
 
+
+import java.util.Scanner;
+
 public class Calculator {
+    double value = 0;
 
     public double execute(String command) {
-        return 0;
+
+        String [] operator = command.split(" ");
+
+        switch(operator[0])
+        {
+            case "+":
+                value += Double.parseDouble(operator[1]);
+                break;
+
+           case "-":
+            value -= Double.parseDouble(operator[1]);
+            break;
+
+            case "*":
+                value *= Double.parseDouble(operator[1]);
+                break;
+
+            case "/":
+                value /= Double.parseDouble(operator[1]);
+                break;
+
+        }
+        return value;
+
+
     }
 }
