74c2ce3e52de4d7881d463a00482e7ce5dd43274
Szymon Basiul <szymon.basiul@wp.pl>
2020-02-01 10:38:27 +0100

    Uncommented unit tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,36 @@ package calculator;
 
 public class Calculator {
 
+    double value = 0;
+
     public double execute(String command) {
-        return 0;
+
+        String[] values = command.split(" ");
+        String operation = values[0];
+        double number = Double.parseDouble(values[1]);
+
+        switch (operation) {
+            case "+": {
+                value+=number;
+            }
+            break;
+            case "-": {
+                value-=number;
+            }
+            break;
+            case "/": {
+                value/=number;
+            }
+            break;
+            case "*": {
+                value*=number;
+            }
+            break;
+        }
+        return value;
+
+
+
+
     }
 }
