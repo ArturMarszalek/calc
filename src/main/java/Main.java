980867bf816847ebfce3a84955d0dbb839fa02d4
Radek <radek.halfaf@gmail.com>
2020-02-01 12:36:51 +0100

    add input func.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import calculator.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.inputDate();
+    }
+}
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,8 +1,27 @@
 package calculator;
 
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Calculator {
     double value = 0;
+
+    public void inputDate() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Proszę podać działanie i liczbę ");
+        String command = sc.nextLine();
+        double value = execute(command);
+        System.out.println(value);
+        if (command.equals("exit")) {
+            System.out.println("Rezultat to : " + value);
+        }
+        else {
+            inputDate();
+        }
+    }
+
+
     public double execute(String command) {
 
         String[] calculation = command.split(" ");
