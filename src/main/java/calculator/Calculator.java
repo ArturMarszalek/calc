4119e24af05df85d3b7d4bd185306d8f2174bdd8
Jareczeq <contact@jareczeq.pl>
2020-02-01 10:23:34 +0100

    Napisano funkcję execute

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,7 +3,42 @@ package calculator;
 
 public class Calculator {
 
+    private double result;
+
+    public Calculator() {
+        result = 0;
+    }
+
     public double execute(String command) {
-        return 0;
+
+        String[] split = command.split(" ");
+
+        double value = Double.parseDouble(split[1]);
+
+        switch (split[0]) {
+
+            case "+":
+                result += value;
+                break;
+
+            case "-":
+                result -= value;
+                break;
+
+            case "*":
+                result *= value;
+                break;
+
+            case "/":
+                result /= value;
+                break;
+
+            default:
+                System.out.println("Nie podano operatora!");
+                break;
+
+        }
+
+        return result;
     }
 }
