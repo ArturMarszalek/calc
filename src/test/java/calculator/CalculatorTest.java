b5d28f1e208b5c3a1e45c49d39f4c2cefe3e0ab4
marszalekartur <marszalekartur@wp.pl>
2020-02-02 09:23:37 +0100

    Add Exception tests

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,43 +15,70 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
-//    @Test
-//    void shouldAddNumber() {
-//        //given
-//
-//        //when
-//        double result = calculator.execute("+ 4");
-//        //then
-//        assertThat(result).isEqualTo(4);
-//    }
-//
-//    @Test
-//    void shouldSubtractNumber() {
-//        //given
-//
-//        //when
-//        double result = calculator.execute("- 7");
-//        //then
-//        assertThat(result).isEqualTo(-7);
-//    }
-//
-//    @Test
-//    void shouldMultiplyNumbers() {
-//        //given
-//        calculator.execute("+ 3.5");
-//        //when
-//        double result = calculator.execute("* 3");
-//        //then
-//        assertThat(result).isEqualTo(10.5);
-//    }
-//
-//    @Test
-//    void shouldDivideNumbers() {
-//        //given
-//        calculator.execute("+ 7.5");
-//        //when
-//        double result = calculator.execute("/ 2.5");
-//        //then
-//        assertThat(result).isEqualTo(3);
-//    }
+    @Test
+    void shouldAddNumber() {
+        //given
+
+        //when
+        double result = calculator.execute("+ 4");
+        //then
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void shouldSubtractNumber() {
+        //given
+
+        //when
+        double result = calculator.execute("- 7");
+        //then
+        assertThat(result).isEqualTo(-7);
+    }
+
+    @Test
+    void shouldMultiplyNumbers() {
+        //given
+        calculator.execute("+ 3.5");
+        //when
+        double result = calculator.execute("* 3");
+        //then
+        assertThat(result).isEqualTo(10.5);
+    }
+
+    @Test
+    void shouldDivideNumbers() {
+        //given
+        calculator.execute("+ 7.5");
+        //when
+        double result = calculator.execute("/ 2.5");
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void shouldThrowAnExceptionWhenPatterIsInvalid() {
+        //given
+        Exception expectedException = null;
+        //when
+        try {
+            calculator.execute("-2");
+        }
+        catch (Exception e)
+        {
+            expectedException = e;
+        }
+        //then
+        assertThat(expectedException).isNotNull();
+    }
+
+    @Test
+    void shouldThrowAnExceptionWhenPatterIsInvalidAgain() {
+        //given
+        //when
+        Exception expectedException = Assertions.assertThrows(Exception.class,
+                () -> calculator.execute("-2"));
+
+        //then
+        assertThat(expectedException).isNotNull();
+    }
 }
